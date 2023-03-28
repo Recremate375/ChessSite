3eb571e1547fbcd94841c482dd6f8be885b229bc
Alexey <70941526+Recremate375@users.noreply.github.com>
2023-03-28 09:25:36 +0300

    Initial Commit

---
+++ ChessSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChessSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
